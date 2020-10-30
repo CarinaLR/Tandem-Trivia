12e593f3841ad31b6b9f6b4512679485775fc6d4
CarinaLR <carinalr84@gmail.com>
2020-10-29 22:36:02 -0400

    sending json response

--- tandem/views.py
+++ tandem/views.py
@@ -1,4 +1,8 @@
-from django.http import HttpResponse
+import json
+from django.http import JsonResponse
+from django.shortcuts import HttpResponse, HttpResponseRedirect, render
+from django.core import serializers
+from django.urls import reverse
 from django.shortcuts import render
 
 from .models import Session, Question, Option
@@ -9,7 +13,10 @@ from .models import Session, Question, Option
 def index(request):
     return render(request, "tandem/index.html")
 
+# Get question data and send a json respond as array of objcs
 
-def questions(request):
-    questionAll = Question.objects.all()
-    print("questions ", questionAll)
+
+def question(request):
+    questionAll = Question.objects.all().order_by('pk')
+    question_serialized = serializers.serialize('json', questionAll)
+    return JsonResponse(question_serialized, safe=False)
